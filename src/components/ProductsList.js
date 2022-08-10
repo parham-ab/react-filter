4314a1ebff0f67d7826483eeacb74b9c6225a5d6
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-10 13:11:41 +0430

    filter products

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,17 +1,49 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+// bootstrap
+import Form from "react-bootstrap/Form";
 // components
 import Product from "./Product";
 
 const ProductsList = () => {
   const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState("");
+  //   base url
+  const BASE_URL = `https://fakestoreapi.com/products`;
+  //  fetch functions
+  const fetchAll = async () => {
+    const response = await axios.get(`${BASE_URL}`);
+    setProducts(response.data);
+  };
+  const fetchJewelry = async () => {
+    const response = await axios.get(`${BASE_URL}/category/jewelery`);
+    setProducts(response.data);
+  };
+  const fetchElectronics = async () => {
+    const response = await axios.get(`${BASE_URL}/category/electronics`);
+    setProducts(response.data);
+  };
+  const fetchmens = async () => {
+    const response = await axios.get(`${BASE_URL}/category/men's clothing`);
+    setProducts(response.data);
+  };
+  const fetchWomens = async () => {
+    const response = await axios.get(`${BASE_URL}/category/women's clothing`);
+    setProducts(response.data);
+  };
+  // fetch all products onMount
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(`https://fakestoreapi.com/products`);
-      setProducts(response.data);
-    };
-    fetchData();
+    fetchAll();
   }, []);
+  // fetch based on category
+  useEffect(() => {
+    category === "All" && fetchAll();
+    category === "jewelery" && fetchJewelry();
+    category === "electronics" && fetchElectronics();
+    category === "men's clothing" && fetchmens();
+    category === "women's clothing" && fetchWomens();
+  }, [category]);
+
   // delete handle
   const deleteHandle = (id) => {
     const filteredItems = products.filter((item) => item.id !== id);
@@ -28,6 +60,18 @@ const ProductsList = () => {
 
   return (
     <div>
+      <Form.Select
+        // defaultValue={"All"}
+        size="sm"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option>All</option>
+        <option>jewelery</option>
+        <option>electronics</option>
+        <option>men's clothing</option>
+        <option>women's clothing</option>
+      </Form.Select>
       {products.map((item) => (
         <Product
           key={item.id}
